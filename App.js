197d8efe510bcea70a6c4e9e98542eeb354ea921
nlmz94 <nmz94140@gmail.com>
2021-12-08 07:49:04 +0100

    idk

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import type { Node } from 'react';
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
